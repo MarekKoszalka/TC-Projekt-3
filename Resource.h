0525c25e9c6fbaaa88da92141759a66f45dc8627
MarekKoszalka <marek.koszalka94@gmail.com>
2015-06-10 00:01:39 +0200

    Create Resource.h

---
+++ Resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Ramie_Robota.rc
+//
+
+#define IDS_APP_TITLE			103
+
+/*MOJE IDENTYFIKATORY*/
+#define ID_BUTTON_UP			140
+#define ID_BUTTON_DOWN			141
+#define ID_BUTTON_LEFT			142
+#define ID_BUTTON_RIGHT			143
+#define ID_BUTTON_CATCH			144
+#define ID_BUTTON_PUT_DOWN		145
+/*KONIEC MOICH IDENTYFIKATORÓW*/
+
+#define IDR_MAINFRAME			128
+#define IDD_RAMIE_ROBOTA_DIALOG	102
+#define IDD_ABOUTBOX			115
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_RAMIE_ROBOTA			107
+#define IDI_SMALL				108
+#define IDC_RAMIE_ROBOTA			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
